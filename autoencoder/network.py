219b66b674035d01c883ac1f49e2fa49bac88343
Hironori Yamamoto <h.yamamoto@ss.cs.osakafu-u.ac.jp>
2018-08-01 16:18:44 +0900

    Fix the decode function.

--- autoencoder/network.py
+++ autoencoder/network.py
@@ -14,16 +14,39 @@ class AutoEncoder(object):
         self.W = np.random.rand(n_in, n_out)
 
     def encode(self, x):
+        """
+        :param x:
+        :return:
+        ```
+            W^{T} x
+        ```
+        """
         z = F.batch_matmul(self.W, x, transa=True, bcasta=True)
 
         return z
 
     def decode(self, z):
-        y = F.batch_matmul(self.W, z, bcasta=True)
+        """
+        :param z:
+        :return:
+        ```
+             W(W^{\mathrm{T}}W)^{-1} z
+        ```
+        """
+        h1 = np.linalg.inv(np.matmul(self.W.T, self.W))
+        h2 = np.matmul(self.W, h1)
+        y = F.batch_matmul(h2, z, bcasta=True)
 
         return y
 
     def reconstruction(self, x):
+        """
+        :param x:
+        :return:
+        ```
+            W(W^{\mathrm{T}}W)^{-1}W^{\mathrm{T}} x
+        ```
+        """
         z = self.encode(x)
         y = self.decode(z)
 
